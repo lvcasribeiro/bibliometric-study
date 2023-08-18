1b8c423e4823c3a8fd4ffe1bd07e434591fa7b5f
Lucas Ribeiro <lucas.and.rib@gmail.com>
2023-08-18 12:03:32 -0300

    Update main python file, adding year analysis.

--- bibliometric_main.py
+++ bibliometric_main.py
@@ -3,6 +3,7 @@ import pandas as pypd
 
 # Files importation:
 import languages_analysis
+import year_analysis
 
 # Reading the .csv file:
 scopus_dataframe = pypd.read_csv(r'C:\Users\lucas\Downloads\scopus-193.csv');
@@ -10,3 +11,4 @@ scopus_dataframe = pypd.read_csv(r'C:\Users\lucas\Downloads\scopus-193.csv');
 # Overall execution:
 if __name__ == '__main__':
     languages_analysis.languages_analysis(scopus_dataframe);
+    year_analysis.year_analysis(scopus_dataframe);
