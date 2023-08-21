0671da5ffa6e7d7b022c5bc27132839f3d0b4023
Lucas Ribeiro <lucas.and.rib@gmail.com>
2023-08-20 21:42:22 -0300

    Update main python file, adding word cloud analysis.

--- bibliometric_main.py
+++ bibliometric_main.py
@@ -6,6 +6,7 @@ import languages_analysis
 import year_analysis
 import evolution_analysis
 import type_analysis
+import wordmap_analysis
 
 # Reading the .csv scopus file:
 scopus_dataframe = pypd.read_csv(r'C:\Users\lucas\Downloads\scopus-193.csv');
@@ -16,3 +17,4 @@ if __name__ == '__main__':
     year_analysis.year_analysis(scopus_dataframe);
     evolution_analysis.evolution_analysis(scopus_dataframe);
     type_analysis.type_analysis(scopus_dataframe);
+    wordmap_analysis.wordmap_analysis(scopus_dataframe);
