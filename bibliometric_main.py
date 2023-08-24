371d827abe278e7fb63cbadec80e4dc19c391ba9
Lucas Ribeiro <lucas.and.rib@gmail.com>
2023-08-23 21:18:36 -0300

    Update main python file, adding new analysis files path.

--- bibliometric_main.py
+++ bibliometric_main.py
@@ -2,14 +2,14 @@
 import pandas as pypd
 
 # Files importation:
-import languages_analysis
-import year_analysis
-import evolution_analysis
-import type_analysis
-import wordmap_analysis
-import keywords_analysis
-import periodics_analysis
-import citations_analysis
+import analysis.languages_analysis as languages_analysis
+import analysis.year_analysis as year_analysis
+import analysis.evolution_analysis as evolution_analysis
+import analysis.type_analysis as type_analysis
+import analysis.wordmap_analysis as wordmap_analysis
+import analysis.keywords_analysis as keywords_analysis
+import analysis.periodics_analysis as periodics_analysis
+import analysis.citations_analysis as citations_analysis
 
 # Reading the .csv scopus file:
 scopus_dataframe = pypd.read_csv(r'C:\Users\lucas\Downloads\scopus-193.csv');
