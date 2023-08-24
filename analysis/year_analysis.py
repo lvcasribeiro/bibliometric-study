0a5018f145e00f1b0fa472ccdd85126e8db7ceab
Lucas Ribeiro <lucas.and.rib@gmail.com>
2023-08-23 21:16:15 -0300

    Update publications per year analysis file.

---
+++ analysis/year_analysis.py
@@ -0,0 +1,18 @@
+# Packages importation:
+import matplotlib.pyplot as pypt
+import numpy as np
+
+# Years analysis:
+def year_analysis(df):
+    years = df['Year'];
+
+    fig, ax = pypt.subplots();
+    ax.barh(years.value_counts().index, years.value_counts().values);
+
+    ax.set_xlabel('Publications per year');
+    ax.grid(axis='x');
+
+    years_list = list(np.arange(years.values.min() - 1, years.values.max() + 2, 1));
+
+    pypt.yticks(years_list);
+    pypt.show();
